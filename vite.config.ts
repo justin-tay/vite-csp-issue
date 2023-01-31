eaf410c569ba28a898b200a4d795576f74e3d5e1
Justin Tay <justin.tay.tw@gmail.com>
2023-01-31 19:25:27 +0800

    feat: issue with style-src csp

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    headers: {
+      'Content-Security-Policy': `style-src 'nonce-random' 'self'`,
+    },
+  },
   plugins: [react()],
 })
